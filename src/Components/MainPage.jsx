dde87752f16e4c0e52b5963f8f444eeb463a7e5e
Artemis <gayatri@artemisinfratech.com>
2025-09-17 10:40:05 +0530

    updated

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -52,8 +52,8 @@ const FlatInventory = () => {
 
 const fetchFlats = async () => {
   try {
-    // const res = await fetch("http://localhost:5000/api/flats");
-    const res = await fetch("/api/flats");
+    const res = await fetch("http://localhost:5000/api/flats");
+    // const res = await fetch("/api/flats");
     let data = await res.json();
     const now = new Date();
 
@@ -137,8 +137,8 @@ const [formData, setFormData] = useState({
     } else {
       payload.holdUntil = null;
     }
-    const res = await fetch(`/api/flats/${selectedFlat.flatKey}`, {
-    // const res = await fetch(`http://localhost:5000/api/flats/${selectedFlat.flatKey}`, {
+    // const res = await fetch(`/api/flats/${selectedFlat.flatKey}`, {
+    const res = await fetch(`http://localhost:5000/api/flats/${selectedFlat.flatKey}`, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(payload),
