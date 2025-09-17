3480136c55639b1aef04730dad53bffe95efae49
Artemis <gayatri@artemisinfratech.com>
2025-09-17 12:31:44 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -8,15 +8,19 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 // app.use(cors());
+// app.use(cors({
+//   origin: [
+//     //  "https://elara-flat-inventory.netlify.app/"],
+//     "https://transcendent-unicorn-5d6a23.netlify.app"], 
+//      // frontend URLs
+//   methods: ["GET", "POST", "PUT"],
+//   credentials: true
+// }));
 app.use(cors({
-  origin: ["http://localhost:5173",
-    //  "https://elara-flat-inventory.netlify.app/"],
-    "https://transcendent-unicorn-5d6a23.netlify.app"], 
-     // frontend URLs
-  methods: ["GET", "POST", "PUT"],
+  origin: ["https://transcendent-centaur-5ba721.netlify.app"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
-
 app.use(express.json());
 
 // Sample route
